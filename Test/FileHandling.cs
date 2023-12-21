15350a96ea95f52cff934877f2d900b5628a465b
Mcn <?>
2023-12-21 20:57:36 +0800

    添加了用于测试字典的项目

---
+++ Test/FileHandling.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class FileHandling
+    {
+        public static void Func()
+        {
+            var fileName = "words";
+            StringR(fileName + ".txt", out List<string> lines);
+            StringW(fileName + "-副本.txt", lines);
+        }
+
+        static void StringR(string fileName, out List<string> lines)
+        {
+            lines = [];
+            if (File.Exists(fileName) == false)
+            {
+                return;
+            }
+
+            using (var sr = new StreamReader(fileName))
+            {
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null)
+                        break;
+
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+
+                    var splits = line.Split(' ');
+
+                    if (splits[0].Length > 1)
+                    {
+                        var punctuation = false;
+                        foreach (var split in splits[0])
+                        {
+                            if (char.IsPunctuation(split))
+                            {
+                                punctuation = true;
+                                break;
+                            }
+                        }
+                        if (punctuation == false)
+                        {
+                            var res = false;
+                            foreach (var item in lines)
+                            {
+                                if (Equals(item, splits[0]))
+                                {
+                                    res = true;
+                                    break;
+                                }
+                            }
+                            if (res == false)
+                            {
+                                lines.Add(splits[0]);
+                            }
+                        }
+                        
+                    }
+                }
+                sr.Close();
+            }
+        }
+
+        static void StringW(string fileName, in List<string> lines)
+        {
+            using (var sw = new StreamWriter(fileName))
+            {
+                foreach (var line in lines)
+                {
+                    var temp = line.ToLower();
+                    if (Regex.IsMatch(temp, "[a-z]"))
+                    {
+                        sw.WriteLine(temp);
+                    }
+                }
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+var word = Console.ReadLine();
+var words = Dictionary.ParseString.GetWordArray(word);
+
+var beginTime = DateTime.Now;
+
+foreach (var item in words)
+{
+    var res = Dictionary.Dictionary.Query(item);
+    if (res != null)
+    {
+        Console.WriteLine(res.en);
+        Console.WriteLine(res.zh);
+    }
+    var endTime = DateTime.Now;
+    var oTime = endTime.Subtract(beginTime);
+    Console.WriteLine(oTime.TotalSeconds);
+}
